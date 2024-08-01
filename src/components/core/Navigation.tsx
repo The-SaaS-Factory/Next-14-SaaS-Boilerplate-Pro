5ee263e4c4cf158993adf5104a42d4abe9b87b22
Yoannis Sánchez Soto <yossthedev@gmail.com>
2024-08-01 05:52:25 -0400

    feat: :sparkles: New SearhcBar

--- src/components/core/Navigation.tsx
+++ src/components/core/Navigation.tsx
@@ -1,9 +1,20 @@
 "use client";
-import { classNames } from "@/utils/facades/serverFacades/strFacade";
-import { useSidebarState } from "@/states/ui/sidebarState";
+
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { useEffect, useState } from "react";
+import {
+  ReactNode,
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import clsx from "clsx";
+import { Tag } from "../ui/Tag";
+import { AnimatePresence, motion, useIsPresent } from "framer-motion";
+import { useSectionStore } from "../ui/SectionProvider";
+import { remToPx } from "@/lib/remToPx";
 
 interface NavigationSection {
   sectionName: string;
@@ -17,11 +28,242 @@ type NavigationItem = {
   current: boolean;
 };
 
-const Navigation = ({ navigation }: { navigation: NavigationSection[] }) => {
-  const { toggleSidebarMenu } = useSidebarState(({ toggleSidebarMenu }) => ({
-    toggleSidebarMenu,
-  }));
+const IsInsideMobileNavigationContext = createContext(false);
 
+export function useIsInsideMobileNavigation() {
+  return useContext(IsInsideMobileNavigationContext);
+}
+
+function useInitialValue<T>(value: T, condition = true) {
+  let initialValue = useRef(value).current;
+  return condition ? initialValue : value;
+}
+
+function NavLink({
+  href,
+  children,
+  tag,
+  active = false,
+  isAnchorLink = false,
+}: {
+  href: string;
+  children: ReactNode;
+  tag?: string;
+  active?: boolean;
+  isAnchorLink?: boolean;
+}) {
+  return (
+    <Link
+      href={href}
+      aria-current={active ? "page" : undefined}
+      className={clsx(
+        "flex justify-between gap-2 py-1 pr-3 text-sm transition",
+        isAnchorLink ? "pl-7" : "pl-4",
+        active
+          ? "text-zinc-900 dark:text-white"
+          : "text-zinc-600 hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
+      )}
+    >
+      <div className="flex gap-2">{children}</div>
+
+      {tag && (
+        <Tag variant="small" color="zinc">
+          {tag}
+        </Tag>
+      )}
+    </Link>
+  );
+}
+
+function VisibleSectionHighlight({
+  group,
+}: {
+  group: NavigationSection;
+  pathname: string;
+}) {
+  let [sections, visibleSections] = useInitialValue(
+    [
+      useSectionStore((s) => s.sections),
+      useSectionStore((s) => s.visibleSections),
+    ],
+    useIsInsideMobileNavigation()
+  );
+
+  const pathName = usePathname();
+  let seg = pathName.split("/");
+  let pathNameWithoutLand = "/" + seg.slice(2).join("/");
+
+  let isPresent = useIsPresent();
+  let firstVisibleSectionIndex = Math.max(
+    0,
+    [{ id: "_top" }, ...sections].findIndex(
+      (section) => section.id === visibleSections[0]
+    )
+  );
+  let itemHeight = remToPx(2);
+  let height = isPresent
+    ? Math.max(1, visibleSections.length) * itemHeight
+    : itemHeight;
+  let top =
+    group.items.findIndex((link) => link.href === pathNameWithoutLand) *
+      itemHeight +
+    firstVisibleSectionIndex * itemHeight;
+
+  return (
+    <motion.div
+      layout
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { delay: 0.2 } }}
+      exit={{ opacity: 0 }}
+      className="absolute inset-x-0 top-0 bg-zinc-800/2.5 will-change-transform dark:bg-white/2.5"
+      style={{ borderRadius: 8, height, top }}
+    />
+  );
+}
+
+function ActivePageMarker({
+  group,
+  pathname,
+}: {
+  group: NavigationSection;
+  pathname: string;
+}) {
+  let seg = pathname.split("/");
+  let pathNameWithoutLand = "/" + seg.slice(2).join("/");
+
+  let itemHeight = remToPx(2);
+  let offset = remToPx(0.25);
+  let activePageIndex = group.items.findIndex(
+    (link) => link.href === pathNameWithoutLand
+  );
+  let top = offset + activePageIndex * itemHeight;
+
+  return (
+    <motion.div
+      layout
+      className="absolute left-2 h-6 w-px bg-[#4437cd]"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1, transition: { delay: 0.2 } }}
+      exit={{ opacity: 0 }}
+      style={{ top }}
+    />
+  );
+}
+
+function NavigationGroup({
+  group,
+  className,
+}: {
+  group: NavigationSection;
+  className?: string;
+}) {
+  // If this is the mobile navigation then we always render the initial
+  // state, so that the state does not change during the close animation.
+  // The state will still update when we re-open (re-render) the navigation.
+  let isInsideMobileNavigation = useIsInsideMobileNavigation();
+  let [pathname, sections] = useInitialValue(
+    [usePathname(), useSectionStore((s) => s.sections)],
+    isInsideMobileNavigation
+  );
+
+  const pathName = usePathname();
+  let seg = pathName.split("/");
+  let pathNameWithoutLand = "/" + seg.slice(2).join("/");
+
+  let isActiveGroup =
+    group.items.findIndex((link) => link.href === pathNameWithoutLand) !== -1;
+
+  return (
+    <li className={clsx("relative mt-6", className)}>
+      <Link
+        href={group.items[0].href}
+        className="flex gap-2 items-center cursor-pointer"
+      >
+        <motion.h2
+          layout="position"
+          className="text-xs font-semibold text-zinc-900 dark:text-white"
+        >
+          {group.sectionName}
+        </motion.h2>
+      </Link>
+
+      {isActiveGroup && (
+        <AnimatePresence key={`menu-${group.sectionName}`}>
+          <motion.div
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{
+              delay: 0.1,
+            }}
+          >
+            <div className="relative mt-3 pl-2">
+              <AnimatePresence initial={!isInsideMobileNavigation}>
+                {isActiveGroup && (
+                  <VisibleSectionHighlight group={group} pathname={pathname} />
+                )}
+              </AnimatePresence>
+              <motion.div
+                layout
+                className="absolute inset-y-0 left-2 w-px bg-zinc-900/10 dark:bg-white/5"
+              />
+              <AnimatePresence initial={false}>
+                {isActiveGroup && (
+                  <ActivePageMarker group={group} pathname={pathname} />
+                )}
+              </AnimatePresence>
+              <ul role="list" className="border-l border-transparent">
+                {group.items.map((link) => (
+                  <motion.li
+                    key={"nav-link" + link.href}
+                    layout="position"
+                    className="relative"
+                  >
+                    <NavLink href={link.href} active={link.href === pathname}>
+                      <>
+                        <link.icon className="h-5 w-5 shrink-0 text-primary-hover"></link.icon>
+                        <span className="truncate"> {link.name}</span>
+                      </>
+                    </NavLink>
+                    <AnimatePresence mode="popLayout" initial={false}>
+                      {link.href === pathname && sections.length > 0 && (
+                        <motion.ul
+                          role="list"
+                          initial={{ opacity: 0 }}
+                          animate={{
+                            opacity: 1,
+                            transition: { delay: 0.1 },
+                          }}
+                          exit={{
+                            opacity: 0,
+                            transition: { duration: 0.15 },
+                          }}
+                        >
+                          {sections.map((section) => (
+                            <li key={section.id}>
+                              <NavLink
+                                href={`${link.href}#${section.id}`}
+                                tag={section.tag}
+                                isAnchorLink
+                              >
+                                {section.title}
+                              </NavLink>
+                            </li>
+                          ))}
+                        </motion.ul>
+                      )}
+                    </AnimatePresence>
+                  </motion.li>
+                ))}
+              </ul>
+            </div>
+          </motion.div>
+        </AnimatePresence>
+      )}
+    </li>
+  );
+}
+
+const Navigation = ({ navigation }: { navigation: NavigationSection[] }) => {
   const pathName = usePathname();
   let seg = pathName.split("/");
   let pathNameWithoutLand = "/" + seg.slice(2).join("/");
@@ -46,40 +288,13 @@ const Navigation = ({ navigation }: { navigation: NavigationSection[] }) => {
 
   return (
     <li>
-      <ul role="list" className="-mx-2 space-y-1">
-        {links.map((section) => (
-          <div key={section.sectionName}>
-            <span className="text-xs font-semibold leading-6 text-primary">
-              {section.sectionName}
-            </span>
-            <ul role="list" className="-mx-2 mt-2 space-y-1">
-              {section.items.map((item) => (
-                <li key={item.name}>
-                  <Link
-                    href={item.href}
-                    onClick={() => toggleSidebarMenu()}
-                    className={classNames(
-                      item.href === pathNameWithoutLand
-                        ? "bg-main-selected text-primary-selected"
-                        : " bg-main-hover",
-                      "group flex gap-x-3 rounded-md p-2  text-primary"
-                    )}
-                  >
-                    <item.icon
-                      className={classNames(
-                        item.href === pathNameWithoutLand
-                          ? "text-primary-selected"
-                          : "text-primary",
-                        "h-6 w-6 shrink-0 text-primary-hover"
-                      )}
-                      aria-hidden="true"
-                    />
-                    <span>{item.name}</span>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
+      <ul role="list" className="space-y-4">
+        {links.map((section, index) => (
+          <NavigationGroup
+            key={`menu-group-${section.sectionName}`}
+            group={section}
+            className={index === 0 ? "md:mt-0" : ""}
+          />
         ))}
       </ul>
     </li>
