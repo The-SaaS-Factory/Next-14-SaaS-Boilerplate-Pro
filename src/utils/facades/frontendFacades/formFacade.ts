fabcf09d7a566b48df6f37fd622117314fa2913c
Royler Marichal <cluzstudio@gmail.com>
2024-08-09 08:06:26 -0300

    feat: Add image upload functionality

--- src/utils/facades/frontendFacades/formFacade.ts
+++ src/utils/facades/frontendFacades/formFacade.ts
@@ -1,6 +1,12 @@
+"use client";
 import { Field } from "@/components/core/NewForm";
 import { SuperAdminSetting } from "@/interfaces/superAdminModule";
+import { constants } from "@/lib/constants";
 import { saveImage } from "@/utils/facades/serverFacades/mediaFacade";
+const url =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000/api/utils/media"
+    : `${constants.appUrl}/api/utils/media`;
 
 export const parseSettingDataOnSubmit = async (data: any, fields: any) => {
   try {
@@ -12,6 +18,7 @@ export const parseSettingDataOnSubmit = async (data: any, fields: any) => {
 
       if (fieldValue !== undefined) {
         if (field.type === "image") {
+          //  console.log(JSON.stringify(fieldValue[0].data_url));
           // return;
           if (
             fieldValue &&
@@ -33,19 +40,15 @@ export const parseSettingDataOnSubmit = async (data: any, fields: any) => {
           for (let i = 0; i < imagesCount; i++) {
             //image with base64 structure
             const image = images[i];
+
             const response = await saveImage(image);
+
             if (response) {
               const responseF = await response.json();
               valueFinal = fieldValue.replace(image, responseF.url);
             } else {
               valueFinal = fieldValue.replace(image, "");
             }
-          }
-        } else if (field.type === "file") {
-          const response = await saveImage(fieldValue);
-          if (response) {
-            const responseF = await response.json();
-            valueFinal = responseF.url;
           }
         } else if (field.type === "text") {
           valueFinal = fieldValue.toString();
@@ -67,115 +70,112 @@ export const parseSettingDataOnSubmit = async (data: any, fields: any) => {
   }
 };
 
-export const parseDataOnSubmit = async (data: any, fields: Field[]) => {
+export const parseDataOnSubmit = async (data: any, fields: any) => {
   const payload: any = {};
-
-  const processedData: any = {};
-
-  for (const key in data) {
-    if (key.includes("_")) {
-      const [fieldName, lang] = key.split("_");
-      if (!processedData[fieldName]) {
-        processedData[fieldName] = {};
-      }
-      processedData[fieldName][lang] = data[key];
-    } else {
-      processedData[key] = data[key];
-    }
-  }
-
-  const promises = fields.flat().map(async (field: Field) => {
+  const promises = fields.map(async (field: any) => {
     const fieldName = field.name;
-    let fieldValue = processedData[fieldName];
+    let fieldValue = data[fieldName];
 
-    payload[fieldName] = field.hasLanguageSupport
-      ? JSON.stringify(fieldValue)
-      : fieldValue;
+    payload[fieldName] = fieldValue;
 
     if (fieldValue !== undefined) {
       if (field.type === "number") {
         payload[fieldName] = parseFloat(fieldValue);
-      } else if (field.type === "file") {
-        const response = await saveImage(fieldValue);
-        if (response) {
-          payload[fieldName] = response.url;
-        }
       } else if (field.type === "image") {
         if (typeof fieldValue === "string" && fieldValue.includes("http")) {
           payload[fieldName] = fieldValue;
         } else {
           if (fieldValue && fieldValue.length > 0) {
-            const response = await saveImage(
-              JSON.stringify((fieldValue[0] as any)?.data_url)
-            );
-            if (response) {
-              payload[fieldName] = response.url;
-            } else {
-              payload[fieldName] = null;
-            }
+            await fetch(url, {
+              method: "POST",
+              body: JSON.stringify(fieldValue[0].data_url),
+              headers: {
+                "Content-Type": "application/json",
+              },
+            })
+              .then((response) => {
+                return response.json();
+              })
+              .then((response) => {
+                if (response) {
+                  payload[fieldName] = response.url;
+                } else {
+                  payload[fieldName] = null;
+                }
+                return response;
+              })
+              .catch(() => {
+                payload[fieldName] = null;
+              });
           }
         }
       } else if (field.type === "gallery") {
         if (fieldValue && fieldValue.length > 0) {
-          const images = fieldValue
+          const imagesWithOutInBase64 = fieldValue.filter(
+            (f: any) => !f.data_url
+          );
+          const imagesInBase64 = fieldValue
             .filter((f: any) => f.data_url)
             .map((image: any) => image.data_url);
 
-          if (images.length > 0) {
-            const response = await saveImage(JSON.stringify(images));
-            let responseF = [response];
-
-            if (response) {
-              //Add image on update without data_url
-              const imagesUpdate = fieldValue.filter((f: any) => !f.data_url);
-              payload[fieldName] = JSON.stringify(
-                responseF.concat(imagesUpdate)
-              );
-            } else {
-              payload[fieldName] = null;
-            }
+          if (imagesInBase64.length > 0) {
+            await fetch(url, {
+              method: "POST",
+              body: JSON.stringify(imagesInBase64),
+              headers: {
+                "Content-Type": "application/json",
+              },
+            })
+              .then((response) => {
+                return response.json();
+              })
+              .then((response) => {
+                if (response) {
+                  //Add image on update without data_url
+                  const imagesUpdate = fieldValue.filter(
+                    (f: any) => !f.data_url
+                  );
+                  payload[fieldName] = JSON.stringify(
+                    response.concat(imagesUpdate)
+                  );
+                } else {
+                  payload[fieldName] = JSON.stringify(imagesWithOutInBase64);
+                }
+              })
+              .catch(() => {
+                payload[fieldName] = null;
+              });
           } else {
             payload[fieldName] = JSON.stringify(fieldValue);
           }
         } else {
           payload[fieldName] = JSON.stringify(fieldValue);
         }
-        // } else if (field.type === "textarea") {
-        //   //count all images base64
-        //   const images = fieldValue
-        //     ? fieldValue.match(/data:image\/[^;]+;base64[^"]*/g)
-        //     : null;
-        //   const imagesCount = images ? images.length : 0;
-        //   //Save each image on server and replace base64 with url
-        //   for (let i = 0; i < imagesCount; i++) {
-        //     //image with base64 structure
-        //     const image = images[i];
+      } else if (field.type === "textarea") {
+        //count all images base64
+        const images = fieldValue
+          ? fieldValue.match(/data:image\/[^;]+;base64[^"]*/g)
+          : null;
+        const imagesCount = images ? images.length : 0;
+        //Save each image on server and replace base64 with url
+        for (let i = 0; i < imagesCount; i++) {
+          //image with base64 structure
+          const image = images[i];
 
-        //     const response = await saveImage(JSON.stringify(image));
+          const response = await saveImage(JSON.stringify(image));
 
-        //     if (response) {
-        //       const responseF = await response.json();
-        //       fieldValue = fieldValue.replace(image, responseF.url);
-        //     } else {
-        //       fieldValue = fieldValue.replace(image, "");
-        //     }
-        //   }
-        //   payload[fieldName] = fieldValue;
-      } else if (field.type === "searchselect" && field.forceInteger) {
-        payload[fieldName] = parseInt(fieldValue);
+          if (response) {
+            const responseF = await response.json();
+            fieldValue = fieldValue.replace(image, responseF.url);
+          } else {
+            fieldValue = fieldValue.replace(image, "");
+          }
+        }
+        payload[fieldName] = fieldValue;
       } else if (field.type === "select" && field.forceInteger) {
         payload[fieldName] = parseInt(fieldValue);
-      } else if (field.type === "date") {
-        const localDate = new Date(fieldValue + "T00:00:00");
-
-        // Verificar si el objeto Date se creó correctamente
-        if (!isNaN(localDate.getTime())) {
-          // Asignar la fecha local al payload
-          payload[fieldName] = localDate;
-        } else {
-          // Manejar el caso en el que la fecha no es válida
-          console.error("Fecha no válida");
-        }
+      } else if (field.type === "searchselect" && field.forceInteger) {
+        payload[fieldName] = parseInt(fieldValue);
       } else if (field.type === "multiselect") {
         if (fieldValue && fieldValue.length > 0) {
           payload[fieldName] = JSON.stringify(fieldValue);
@@ -189,4 +189,45 @@ export const parseDataOnSubmit = async (data: any, fields: Field[]) => {
   await Promise.all(promises);
 
   return payload;
+};
+
+export const getGalleryImagesUrls = async (dataRaw) => {
+  if (!dataRaw) return;
+  let data;
+  const imagesWithOutInBase64 = dataRaw.filter((f: any) => !f.data_url);
+  const imagesInBase64 = dataRaw
+    .filter((f: any) => f.data_url)
+    .map((image: any) => image.data_url);
+
+  if (imagesInBase64.length > 0) {
+    await fetch(url, {
+      method: "POST",
+      body: JSON.stringify(imagesInBase64),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        if (response) {
+          //Add image on update without data_url
+          const newImages = response.map((image: any) => image.url);
+          const imagesUpdate = dataRaw.filter((f: any) => !f.data_url);
+          data = JSON.stringify(newImages.concat(imagesUpdate));
+        } else {
+          data = JSON.stringify(imagesWithOutInBase64);
+        }
+      })
+      .catch(() => {
+        data = null;
+      });
+  } else {
+    data = JSON.stringify(dataRaw);
+  }
+
+  console.log(data);
+
+  return data;
 };
